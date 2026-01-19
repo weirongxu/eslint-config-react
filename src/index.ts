76bfbb0afbc7b8be808d78074894cfa4f63bf6fa
Weirong Xu <weirongxu.raidou@gmail.com>
2026-01-19 17:57:40 +0800

    feat: upgrade deps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import {
-  ConfigArray,
-  jsconfig,
-  tsconfig,
-  tseslint,
-} from '@raidou/eslint-config-base';
+import { jsconfig, tsconfig, tseslint } from '@raidou/eslint-config-base';
 import { Linter } from 'eslint';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -11,21 +6,16 @@ import { defineConfig } from 'eslint/config';
 
 const reactJsconfig: Linter.Config[] = defineConfig(
   ...jsconfig,
-  reactHooks.configs['recommended-latest'],
+  reactHooks.configs.flat.recommended,
   reactPlugin.configs.flat.recommended,
   reactPlugin.configs.flat['jsx-runtime'],
 );
 
-const reactTsconfig: ConfigArray = tseslint.config(
+const reactTsconfig: Linter.Config[] = defineConfig(
   tsconfig,
-  reactHooks.configs['recommended-latest'],
+  reactHooks.configs.flat.recommended,
   reactPlugin.configs.flat.recommended,
   reactPlugin.configs.flat['jsx-runtime'],
 );
 
-export {
-  reactJsconfig as jsconfig,
-  reactTsconfig as tsconfig,
-  tseslint,
-  type ConfigArray,
-};
+export { reactJsconfig as jsconfig, reactTsconfig as tsconfig, tseslint };
